2f744e7c65bf9e6e2b0fe62b91e0797fbdd16142
rishisoni25 <rishisoni25@gmail.com>
2018-08-11 06:43:17 +0000

    new_files

--- upload_on_git.sh
+++ upload_on_git.sh
@@ -3,7 +3,8 @@ date1=`date`
 successfile='Succes_file'
 failure='failed_file'
 cd  /home/ubuntu/RISHI/volumegit
-git commit -am "new_files $date" && git push origin master
+git add .
+git commit -m "new_files $date" && git push origin master
 if test $? -eq 0
 then
 echo 'Success' >$successfile_$date1.txt
